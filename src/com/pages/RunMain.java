18280b2fd154fc4443038ea3fb4c44ea1e5aab57
Master mind <dhage.pravin222@gmail.com>
2016-12-08 14:49:43 +0530

    there are some changes in project

---
+++ src/com/pages/RunMain.java
@@ -0,0 +1,15 @@
+package com.pages;
+
+public class RunMain {
+
+	public static void main(String[] args) {
+	SalesData salesData=new SalesData();
+	display();
+	salesData.record();
+
+	}
+	public static void display(){
+		System.out.println("welcome to mand aadee ,selffish ,tawleeeeee");
+	}
+
+}
---
+++ src/com/pages/SalesData.java
@@ -0,0 +1,8 @@
+package com.pages;
+
+public class SalesData {
+	public void record(){
+		System.out.println("Aditi talegwakr");
+	}
+	
+}
